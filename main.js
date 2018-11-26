7788680ea388f2c432542e36ea4f7a3a2d17eaf4
dek <deeeqhaji@gmail.com>
2018-11-26 13:45:35 -0600

    campaign data

---
+++ main.js
@@ -0,0 +1,45 @@
+const Nashville = {
+    platform: ['Taxes', 'Jobs','Infrastructure','Health care','Crime and enforcement'],
+    donation: ['http://flexboxfroggy.com/'],
+    eventsCalender: 
+    [{
+        Date:11/12/2018,
+        time: '11AM-11PM',
+        activities1: 'answering phones',
+    }, 
+     {
+        Date:11/13/2018,
+        time: '11AM-11PM',
+        activities1: 'answering follow-up questions',
+    }, 
+    {
+        Date:11/16/2018,
+        time: '11AM-11PM',
+        activities1: 'answering emails',
+    },
+],
+         
+    volunteerInfo: [
+        {
+            name: 'dek',
+            address: '500 interstate blvd nashville Tn',
+            email: 'dek.123@gmail.com',
+            phone:'6158713633',
+            Availibility: 'monday-friday',
+            activities: 'email follow-ups',
+        }
+    ],
+    Biogrophy: ['Elizabeth Ann Warren (née Herring, born June 22, 1949) is an American politician and academic serving as the senior United States Senator from Massachusetts, a seat she has held since 2013. Warren was formerly a professor of law, and taught at the University of Texas School of Law, the University of Pennsylvania Law School, and most recently at Harvard Law School. A prominent scholar specializing in bankruptcy law, Warren was among the most cited law professors in the field of commercial law before she began her political career.'],
+    gallery: [ 
+        {
+            headshot: URL='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Elizabeth_Warren%2C_official_portrait%2C_114th_Congress.jpg/220px-Elizabeth_Warren%2C_official_portrait%2C_114th_Congress.jpg',
+            familyPic: URL='https://media.vanityfair.com/photos/55a66bdffff2c16856a6bc55/master/w_768,c_limit/elizabeth-warren-husband-proposal.jpg',
+            constituentsPic: URL='https://c1.staticflickr.com/5/4560/24052790247_47af80d507_b.jpg',
+}
+],
+    missionStatement:URL='https://slideplayer.com/slide/8064140/25/images/3/Mission+Statement+and+Beliefs.jpg',
+    vote: URL='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects',
+}
+function addToImageGalery ( ) {
+    
+}
